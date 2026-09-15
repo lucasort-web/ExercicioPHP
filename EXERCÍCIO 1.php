1795f153f101326b67ecaf639aaeb554f4579eb6
lucasort-web <l.tavaresort@gmail.com>
2026-09-15 16:47:23 -0300

    exercicio1

---
+++ EXERCÍCIO 1.php
@@ -0,0 +1,16 @@
+EXERCÍCIO 1-
+<?php
+
+$cor = "vermelho";
+
+if ($cor == "vermelho") {
+    echo "Pare!";
+} elseif ($cor == "amarelo") {
+    echo "Atenção!";
+} elseif ($cor == "verde") {
+    echo "Siga!";
+} else {
+    echo "Cor inválida.";
+}
+
+?>
